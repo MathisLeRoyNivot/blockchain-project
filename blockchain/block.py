15dd578121c464dca3ef09711210427194c3a76a
mathislrn <mathis.lrn061198@gmail.com>
2020-01-27 21:03:31 +0100

    feat(blockchain): updating blockchain.py + adding block.py object

---
+++ blockchain/block.py
@@ -0,0 +1,15 @@
+# Block object
+import datetime
+
+
+class Block:
+    block_num = 0
+    data = None
+    hash = None
+    nonce = 0
+    previous_hash = 0x0
+    timestamp = datetime.datetime.now()
+
+    def __init__(self, data):
+        self.data = data
+
--- blockchain/blockchain.py
+++ blockchain/blockchain.py
@@ -1,23 +1,16 @@
 # Blockchain algorithm
 from hashlib import sha256
 from PyPDF2 import PdfFileReader
-import numpy as np
+
 
 PAGE_ITERATION = 5
-BLOCK_LOOP_ITERATIONS = 0
 
 pdf_file_path = input("Which file you want to proceed into the blockchain ? (copy/paste file path)\n")
-# pdf_file_page_number = int(input("Page # ?\n"))
+pdf_file_page_number = int(input("Page no. ?\n"))
 
 pdf_file = open(pdf_file_path, 'rb')
-pages_list = list()
+pages_list_selected = list()
 pages_range = list()
-
-
-def get_page_content(file, page_num):
-    pdf_reader = PdfFileReader(file)
-    page_content = pdf_reader.getPage(page_num).extractText()
-    return page_content
 
 
 def get_pdf_num_pages(file):
@@ -26,35 +19,34 @@ def get_pdf_num_pages(file):
     return num_pages
 
 
-def hash_page_content(file, page_num):
+def get_page_content(file, page_num):
     pdf_reader = PdfFileReader(file)
     page_content = pdf_reader.getPage(page_num).extractText()
+    return page_content
+
+
+def hash_page_content(file, page_num):
+    page_content = get_page_content(file, page_num)
     hash_content = sha256(bytes(page_content, "utf8")).hexdigest()
-    print("Page # : " + str(page_num + 1))
+    print("Page # : " + str(page_num))
     print("Content hash : " + hash_content)
-    print("Content preview : " + page_content[0:50] + "...")
+    print("Content preview : " + page_content[0:100] + "...")
 
 
-def page_treatment(file):
-    page_number = get_pdf_num_pages(file)
-    page_range = np.arange(0, page_number, PAGE_ITERATION)
+def pages_treatment(file, page):
+    if page % PAGE_ITERATION == 0:
+        num_pages = get_pdf_num_pages(file)
 
-    for i in page_range:
-        pages_range.clear()
-        for n in range(i, i + PAGE_ITERATION):
-            if n <= page_number:
-                pages_range.append(n)
-        print(pages_range)
-        print("--------")
-    # pdf_reader = PdfFileReader(file)
+        for i in range(page, page + PAGE_ITERATION):
+            if i <= num_pages:
+                pages_list_selected.append(i)
+                hash_page_content(file, i)
+    else:
+        print("Error !")
 
 
-page_treatment(pdf_file_path)
-# print(pages_list)
-
-# Loop through each page and execute hash function
-#for i in range(get_pdf_num_pages(pdf_file)):
-#    hash_page_content(pdf_file, i)
+# Select only requested pages
+pages_treatment(pdf_file_path, pdf_file_page_number)
 
 # Close opened file
 pdf_file.close()
